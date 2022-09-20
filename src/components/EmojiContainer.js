40dbdfb965d1efa16e371b553d0db5d6b033685d
Sunnykumar17 <98548654+Sunnykumar17@users.noreply.github.com>
2022-09-20 12:45:32 +0530

    Add files via upload

---
+++ src/components/EmojiContainer.js
@@ -0,0 +1,15 @@
+
+import SingleEmoji from "./SingleEmoji";
+const EmojiContainer = ({ list }) => {
+ 
+  return (
+    <div className="container">
+      {list.map((singleEmoji, idx) => {
+        return (
+            <SingleEmoji key={idx} singleEmoji = {singleEmoji}/>
+        )
+      })}
+    </div>
+  )
+}
+export default EmojiContainer;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+const Nav = () => {
+    return (
+        <nav>
+            <h2>Emojee</h2>
+            <a href="/" target = "_blank">Github</a>
+        </nav>
+    )
+}
+
+export default Nav;
+
